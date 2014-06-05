ba18e7059e1dc7fe0e7b813ff2b663cb56286122
firstforest <h.morikosi@gmail.com>
2014-06-06 00:04:02 +0900

    コードを整形する

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,22 @@
 'use strict';
 
 angular.module('enqApp')
-  .controller('MainCtrl',function ($scope, Kiicloud) {
-    $scope.votes = [];
-    
-    $scope.plusfunc = function(){
-      $scope.votes.push({number: 0, p_evaluation: 3, s_evaluation: 3, impression: ""});
-    };
-    
-    $scope.minusfunc = function(){
-      $scope.votes.pop();
-    };
-    
-    for(var i=0; i<4; i++){
-      $scope.plusfunc();
-    }
-    
-	  $scope.post = function(){
-	    Kiicloud.post($scope);
-	  };
-  });
+    .controller('MainCtrl', function ($scope, Kiicloud) {
+        $scope.votes = [];
+
+        $scope.plusfunc = function () {
+            $scope.votes.push({number: 0, p_evaluation: 3, s_evaluation: 3, impression: ""});
+        };
+
+        $scope.minusfunc = function () {
+            $scope.votes.pop();
+        };
+
+        for (var i = 0; i < 4; i++) {
+            $scope.plusfunc();
+        }
+
+        $scope.post = function () {
+            Kiicloud.post($scope);
+        };
+    });
